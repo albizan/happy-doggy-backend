cabef468350e7fd65791eb43e4301c7df0ab09f4
Alberto Zanotti <albizan@hotmail.it>
2019-06-11 00:11:55 +0200

    Add login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get, Post, Body, Logger, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dtos/registerUser.dto';
+import { LoginUserDto } from './dtos/loginUser.dto';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() data: RegisterUserDto): Promise<string> {
+  async register(@Body() data: RegisterUserDto): Promise<string> {
     return this.authService.register(data);
+  }
+
+  @Post('login')
+  async login(@Body() data: LoginUserDto): Promise<string> {
+    return this.authService.login(data);
   }
 }
