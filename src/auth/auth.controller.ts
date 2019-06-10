cf3ed0177ab3db6927b425c822b7b722f80a70e9
Alberto Zanotti <albizan@hotmail.it>
2019-06-10 22:18:47 +0200

    Added register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger, Param } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dtos/registerUser.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() data: RegisterUserDto): Promise<string> {
+    return this.authService.register(data);
+  }
+}
