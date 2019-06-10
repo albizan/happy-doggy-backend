fe67bb359b119391580509be531c24925ff0d14e
Alberto Zanotti <albizan@hotmail.it>
2019-06-10 22:18:26 +0200

    Load env variables with dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(Number(process.env.HTTP_PORT));
+  console.log('App running on port', process.env.HTTP_PORT);
 }
 bootstrap();
